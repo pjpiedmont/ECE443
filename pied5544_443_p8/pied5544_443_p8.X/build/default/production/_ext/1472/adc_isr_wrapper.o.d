acfc9882e7c355e873a298766238a31cf662dc7c
Parker Piedmont <pjpiedmont@gmail.com>
2021-12-06 01:26:00 -0800

    Project 8: Init

---
+++ pied5544_443_p8/pied5544_443_p8.X/build/default/production/_ext/1472/adc_isr_wrapper.o.d
@@ -0,0 +1,9 @@
+build/default/production/_ext/1472/adc_isr_wrapper.o:  \
+ ../adc_isr_wrapper.S  \
+ ../../../FreeRTOS/Source/portable/MPLAB/PIC32MX/ISR_Support.h  \
+ ../FreeRTOSConfig.h 
+build/default/production/_ext/1472/adc_isr_wrapper.o:  \
+ ../FreeRTOSConfig.h  \
+ ../../../FreeRTOS/Source/portable/MPLAB/PIC32MX/ISR_Support.h  \
+  \
+../adc_isr_wrapper.S  \
